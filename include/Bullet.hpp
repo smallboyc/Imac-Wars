bf648acd2ce4d013735a425d97e5507ad088d221
AnassDoublal <a.doublal02@gmail.com>
2024-06-08 19:08:43 +0200

    bullets

--- include/Bullet.hpp
+++ include/Bullet.hpp
@@ -6,11 +6,16 @@
 #include "utils.hpp"
 #include <vector>
 #include "GLHelpers.hpp"
+#include "Enemy.hpp"
 
 struct Bullet
 {
     GLuint texture;
+    glm::vec2 pos;
+    glm::vec2 direction;
+    bool fixedDirection{false};
+    bool isBeingShot{false};
     void setup(std::unordered_map<std::filesystem::path, GLuint> &LoadedTextures);
-    void update();
+    void update(Enemy&);
     void draw(Map&);
 };
--- include/Tower.hpp
+++ include/Tower.hpp
@@ -16,8 +16,9 @@ struct Tower
     int type;
     glm::vec2 pos;
     GLuint texture;
-    std::vector<Bullet> bullets;
+    float fireRate{3};
+    Bullet bullet;
     void setup(std::unordered_map<std::filesystem::path, GLuint> &LoadedTextures);
-    void update(const double &elapsedTime, const double &currentTime, TowerDefense*);
+    void update(const double& elapsedTime, const double &currentTime, TowerDefense*);
     void draw(Map& map);
 };
